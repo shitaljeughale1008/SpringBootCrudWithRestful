1cc5a03aa10172469edfefcf7356726a47c9fb8f
shitaljeughale1008 <shitaljeughale1008@gmail.com>
2022-08-11 17:15:55 +0530

    SpringBootCrud priject added

---
+++ SpringBootCrudWithRestFul/src/main/java/com/BikkadIt/SpringBootCrudWithRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWithRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudWithRestFulApplication.class, args);
+	}
+
+}
